9380b601c454d92bae79e16879ce77e693d658c5
ozgurozbey <ozgurozbey921@gmail.com>
2024-08-08 18:16:49 +0300

    ilk flowgorithm

---
+++ .vscode/extensions/adolfdaniel.vscode-chromium-vector-icons-1.0.2/CHANGELOG.md
@@ -0,0 +1,16 @@
+# Change Log
+
+All notable changes to the "chromium-vector-icons-viewer" extension will be documented in this file.
+
+## [1.0.0]
+
+- Initial release.
+
+## [1.0.1]
+
+- Fixed CSS issue in the preview page.
+- Preview current page on active file change.
+
+## [1.0.2]
+
+- Added an icon for the extension.
---
+++ .vscode/extensions/adolfdaniel.vscode-chromium-vector-icons-1.0.2/out/extension.js
@@ -0,0 +1,103 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deactivate = exports.activate = void 0;
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require("vscode");
+const path = require("path");
+let panel = null;
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+function activate(context) {
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerCommand('chromium-vector-icons-viewer.previewIcon', () => {
+        // The code you place here will be executed every time your command is
+        // executed
+        const activeEditor = vscode.window.activeTextEditor;
+        if (!activeEditor) {
+            console.error('Cannot get the active editor');
+            return;
+        }
+        showPanel(activeEditor, context.extensionUri);
+    });
+    vscode.window.onDidChangeActiveTextEditor((e) => onActiveEditorChanged(e, context.extensionUri));
+    context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+function showPanel(editor, extensionUri) {
+    if (!editor.document.fileName.endsWith('.icon')) {
+        return;
+    }
+    if (panel) {
+        panel.reveal(vscode.ViewColumn.Beside, true);
+        panel.title = `Previewing ${path.basename(editor.document.fileName)}`;
+    }
+    else {
+        panel = vscode.window.createWebviewPanel('previewingVectorIcon', `Previewing ${path.basename(editor.document.fileName)}`, {
+            viewColumn: vscode.ViewColumn.Beside,
+            preserveFocus: true,
+        }, {
+            // Enable javascript in the webview
+            enableScripts: true,
+            // And restrict the webview to only loading content from our extension's `media` directory.
+            localResourceRoots: [vscode.Uri.joinPath(extensionUri, 'media')]
+        });
+        panel.onDidDispose(() => {
+            panel = null;
+        });
+    }
+    // And set its HTML content
+    panel.webview.html = getWebviewContent(panel.webview, extensionUri, editor.document.getText());
+}
+function onActiveEditorChanged(editor, extensionUri) {
+    if (!editor) {
+        return;
+    }
+    if (!panel) {
+        return;
+    }
+    showPanel(editor, extensionUri);
+    console.log("Previewing file" + editor.document.fileName);
+}
+function getWebviewContent(webview, extensionUri, data) {
+    // Local path to main script run in the webview
+    const scriptPathOnDisk = vscode.Uri.joinPath(extensionUri, 'media', 'vector-icon.js');
+    const scriptUri = webview.asWebviewUri(scriptPathOnDisk);
+    // Local path to css styles
+    const stylePath = webview.asWebviewUri(vscode.Uri.joinPath(extensionUri, 'media', 'vector-icon.css'));
+    // Use a nonce to only allow specific scripts to be run
+    const nonce = getNonce();
+    return `<!DOCTYPE html>
+  <html lang='en'>
+  <head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Skia Vector Icon Preview</title>
+
+    <link href='${stylePath}' rel='stylesheet'>
+  </head>
+  <body>
+    <div id='preview-container'>
+    </div>
+    <script nonce='${nonce}' src='${scriptUri}'></script>
+    <script>
+      const data = \`${data}\`;
+      setUpPreviewPanel(data);
+    </script>
+  </body>
+  </html>`;
+}
+function getNonce() {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < 32; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+// this method is called when your extension is deactivated
+function deactivate() { }
+exports.deactivate = deactivate;
+//# sourceMappingURL=extension.js.map
